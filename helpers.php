df2348471fe6cdfdf0b783ac0c372122b6793c86
entimm <entimm@gmail.com>
2017-07-26 14:10:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- helpers.php
+++ helpers.php
@@ -1,8 +1,17 @@
 <?php
 
+/*
+ * This file is part of the entimm/hm.
+ *
+ * (c) entimm <entimm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 use Illuminate\Container\Container;
 
-if (! function_exists('app')) {
+if (!function_exists('app')) {
     function app($abstract = null, array $parameters = [])
     {
         if (is_null($abstract)) {
@@ -15,35 +24,35 @@ if (! function_exists('app')) {
     }
 }
 
-if (! function_exists('mysql_query')) {
+if (!function_exists('mysql_query')) {
     function mysql_query($query)
     {
         return app('mysql')->query($query);
     }
 }
 
-if (! function_exists('mysql_fetch_array')) {
+if (!function_exists('mysql_fetch_array')) {
     function mysql_fetch_array($result)
     {
         return $result->fetch_array();
     }
 }
 
-if (! function_exists('mysql_fetch_assoc')) {
+if (!function_exists('mysql_fetch_assoc')) {
     function mysql_fetch_assoc($result)
     {
         return $result->fetch_assoc();
     }
 }
 
-if (! function_exists('mysql_insert_id')) {
+if (!function_exists('mysql_insert_id')) {
     function mysql_insert_id()
     {
         return app('mysql')->insert_id;
     }
 }
 
-if (! function_exists('mysql_real_escape_string')) {
+if (!function_exists('mysql_real_escape_string')) {
     function mysql_real_escape_string($escapestr)
     {
         return app('mysql')->real_escape_string($escapestr);
