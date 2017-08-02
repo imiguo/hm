07cba26c8bfbf8627f37eddff3236bf77b17127a
entimm <entimm@gmail.com>
2017-08-02 13:55:11 +0800

    分离 index.php 代码

--- http/index.php
+++ http/index.php
@@ -10,6 +10,7 @@
  */
 
 require '../lib/config.inc.php';
+require '../lib/function.php';
 
 $userinfo = [];
 $settings = [];
@@ -22,198 +23,30 @@ if (HTTPS) {
 }
 
 if (isset($frm['ref']) && $frm['ref'] != '') {
-    setcookie('Referer', $frm['ref'], time() + 630720000);
-    if ($frm_cookie['Referer'] == '') {
-        $ref = quote($frm['ref']);
-        $q = 'select id from hm2_users where username = \''.$ref.'\'';
-        ($sth = db_query($q));
-        while ($row = mysql_fetch_array($sth)) {
-            $ref_id = $row['id'];
-            $q = 'select * from hm2_referal_stats where date = current_date() and user_id = '.$ref_id;
-            ($sth = db_query($q));
-            $f = 0;
-            while ($row = mysql_fetch_array($sth)) {
-                $f = 1;
-            }
-
-            if ($f == 0) {
-                $q = 'insert into hm2_referal_stats set date = current_date(), user_id = '.$ref_id.', income = 1, reg = 0';
-                $sth = db_query($q);
-            } else {
-                $q = 'update hm2_referal_stats set income = income+1 where date = current_date() and user_id = '.$ref_id.' ';
-                $sth = db_query($q);
-            }
-
-            break;
-        }
-    }
-
-    if ($settings['redirect_referrals'] != '') {
-        header('Location: '.$settings['redirect_referrals']);
-        exit;
-    }
-}
-
-if (!empty($frm_env['HTTPS'])) {
-    $settings[SSL_USED] = 1;
+    bind_ref();
 }
 
 if ((empty($frm_env['HTTPS']) and isset($settings['redirect_to_https']) and $settings['redirect_to_https'] == 1)) {
-    $url = 'https://'.$frm_env['HTTP_HOST'].$frm_env['SCRIPT_NAME'];
-    if ($env_frm['QUERY_STRING']) {
-        $url .= $env_frm['QUERY_STRING'];
-    }
-
-    header('Location: '.$url);
+    redirect_https();
     exit;
 }
 
-$q = 'select * from hm2_processings';
-($sth = db_query($q));
-while ($row = mysql_fetch_array($sth)) {
-    $sfx = strtolower($row['name']);
-    $sfx = preg_replace('/([^\\w])/', '_', $sfx);
-    $exchange_systems[$row['id']] = [
-        'name'        => $row['name'],
-        'sfx'         => $sfx,
-        'status'      => $row['status'],
-        'has_account' => 0,
-    ];
-}
-
 if ((isset($frm['CUSTOM2']) && $frm['CUSTOM2'] == 'pay_withdraw_eeecurrency' and $frm['TRANSACTION_ID'] != '')) {
-    $batch = $frm['TRANSACTION_ID'];
-    list($id, $str) = explode('-', $frm['CUSTOM1']);
-    $id = sprintf('%d', $id);
-    if ($str == '') {
-        $str = 'abcdef';
-    }
-
-    $str = quote($str);
-    $q = 'select * from hm2_history where id = '.$id.' and str = \''.$str.'\'';
-    $sth = db_query($q);
-    while ($row = mysql_fetch_array($sth)) {
-        $q = 'delete from hm2_history where id = '.$id;
-        (db_query($q));
-        $q = 'insert into hm2_history set
-        user_id = '.$row['user_id'].',
-        amount = -'.abs($row['amount']).(',
-        type = \'withdrawal\',
-        description = \'Withdraw processed. Batch id = '.$batch.'\',
-        actual_amount = -').abs($row['amount']).',
-        ec = 8,
-        date = now()
-        ';
-        (db_query($q));
-        $q = 'select * from hm2_users where id = '.$row['user_id'];
-        $sth = db_query($q);
-        $userinfo = mysql_fetch_array($sth);
-        $info = [];
-        $info['username'] = $userinfo['username'];
-        $info['name'] = $userinfo['name'];
-        $info['amount'] = sprintf('%.02f', abs($row['amount']));
-        $info['account'] = $frm['SELLERACCOUNTID'];
-        $info['batch'] = $batch;
-        $info['paying_batch'] = $batch;
-        $info['receiving_batch'] = $batch;
-        $info['currency'] = $exchange_systems[8]['name'];
-        send_template_mail('withdraw_user_notification', $userinfo['email'], $settings['system_email'], $info);
-    }
-
+    custom2_pay_withdraw_eeecurrency();
     echo 1;
     exit;
 }
 
 if ((isset($frm['CUSTOM2']) && $frm['CUSTOM2'] == 'pay_withdraw' and $frm['TRANSACTION_ID'] != '')) {
-    $batch = $frm['TRANSACTION_ID'];
-    list($id, $str) = explode('-', $frm['CUSTOM1']);
-    $id = sprintf('%d', $id);
-    if ($str == '') {
-        $str = 'abcdef';
-    }
-
-    $str = quote($str);
-    $q = 'select * from hm2_history where id = '.$id.' and str = \''.$str.'\'';
-    $sth = db_query($q);
-    while ($row = mysql_fetch_array($sth)) {
-        $q = 'delete from hm2_history where id = '.$id;
-        (db_query($q));
-        $q = 'insert into hm2_history set
-        user_id = '.$row['user_id'].',
-        amount = -'.abs($row['amount']).(',
-        type = \'withdrawal\',
-        description = \'Withdraw processed. Batch id = '.$batch.'\',
-        actual_amount = -').abs($row['amount']).',
-        ec = 2,
-        date = now()';
-        (db_query($q));
-        $q = 'select * from hm2_users where id = '.$row['user_id'];
-        $sth = db_query($q);
-        $userinfo = mysql_fetch_array($sth);
-        $info = [];
-        $info['username'] = $userinfo['username'];
-        $info['name'] = $userinfo['name'];
-        $info['amount'] = sprintf('%.02f', abs($row['amount']));
-        $info['account'] = $frm['SELLERACCOUNTID'];
-        $info['batch'] = $batch;
-        $info['paying_batch'] = $batch;
-        $info['receiving_batch'] = $batch;
-        $info['currency'] = $exchange_systems[2]['name'];
-        send_template_mail('withdraw_user_notification', $userinfo['email'], $settings['system_email'], $info);
-    }
-
+    custom2_pay_withdraw();
     echo 1;
     exit;
 }
 
 if (((isset($frm['user3']) and $frm['user3'] == 'pay_withdraw' and $frm['transaction_id'] != '') and $frm['transaction_type'] == 'Payment')) {
-    $batch = $frm['transaction_id'];
-    list($id, $str) = explode('-', $frm['user1']);
-    $id = sprintf('%d', $id);
-    if ($str == '') {
-        $str = 'abcdef';
-    }
-
-    $str = quote($str);
-    $q = 'select * from hm2_history where id = '.$id.' and str=\''.$str.'\'';
-    $sth = db_query($q);
-    while ($row = mysql_fetch_array($sth)) {
-        $q = 'delete from hm2_history where id = '.$id;
-        (db_query($q));
-        $q = 'insert into hm2_history set
-        user_id = '.$row['user_id'].',
-        amount = -'.abs($row['amount']).(',
-        type = \'withdrawal\',
-        description = \'Withdraw processed. Batch id = '.$batch.'\',
-        actual_amount = -').abs($row['amount']).',
-        ec = 4,
-        date = now()
-        ';
-        (db_query($q));
-        $q = 'select * from hm2_users where id = '.$row['user_id'];
-        $sth = db_query($q);
-        $userinfo = mysql_fetch_array($sth);
-        $info = [];
-        $info['username'] = $userinfo['username'];
-        $info['name'] = $userinfo['name'];
-        $info['amount'] = sprintf('%.02f', abs($row['amount']));
-        $info['account'] = $frm['payee_email'];
-        $info['batch'] = $batch;
-        $info['paying_batch'] = $batch;
-        $info['receiving_batch'] = $batch;
-        $info['currency'] = $exchange_systems[2]['name'];
-        send_template_mail('withdraw_user_notification', $userinfo['email'], $settings['system_email'], $info);
-    }
-
+    user3_pay_withdraw_payment();
     echo 1;
     exit;
-}
-
-if ($settings['ssl_url'] != '') {
-    if ($SERVER_PORT == 80) {
-        header('Location: '.$settings['ssl_url'].'/');
-        exit;
-    }
 }
 
 if ($frm['a'] == 'run_crontab') {
@@ -225,6 +58,7 @@ $q = 'delete from hm2_online where ip=\''.$frm_env['REMOTE_ADDR'].'\' or date + 
 (db_query($q));
 $q = 'insert into hm2_online set ip=\''.$frm_env['REMOTE_ADDR'].'\', date = now()';
 (db_query($q));
+
 $userinfo = [];
 $userinfo['logged'] = 0;
 if ($frm['a'] == 'logout') {
@@ -249,89 +83,7 @@ if ($settings['crontab_stats'] == 1) {
     $stats = unserialize($s[0]);
 }
 
-if ($settings['show_info_box_members_online'] == 1) {
-    if ($settings['crontab_stats'] == 1) {
-        $settings['show_info_box_members_online_generated'] = $stats['visitors'];
-    } else {
-        $q = 'select count(*) as col from hm2_users where last_access_time + interval 30 minute > now()';
-        ($sth = db_query($q));
-        $row = mysql_fetch_array($sth);
-        $settings['show_info_box_members_online_generated'] = $row['col'];
-    }
-}
-
-if ($settings['show_info_box_total_accounts'] == 1) {
-    if ($settings['crontab_stats'] == 1) {
-        $settings['info_box_total_accounts_generated'] = $stats['total_users'];
-    } else {
-        $q = 'select count(*) as col from hm2_users where id > 1';
-        ($sth = db_query($q));
-        $row = mysql_fetch_array($sth);
-        $settings['info_box_total_accounts_generated'] = $row['col'];
-    }
-}
-
-if ($settings['show_info_box_active_accounts'] == 1) {
-    if ($settings['crontab_stats'] == 1) {
-        $settings['info_box_total_active_accounts_generated'] = $stats['active_accounts'];
-    } else {
-        $q = 'select count(distinct user_id) as col from hm2_deposits ';
-        ($sth = db_query($q));
-        $row = mysql_fetch_array($sth);
-        $settings['info_box_total_active_accounts_generated'] = $row['col'];
-    }
-}
-
-if ($settings['show_info_box_vip_accounts'] == 1) {
-    $q = 'select count(distinct user_id) as col from hm2_deposits where actual_amount > '.sprintf('%.02f',
-            $settings['vip_users_deposit_amount']);
-    ($sth = db_query($q));
-    $row = mysql_fetch_array($sth);
-    $settings['info_box_total_vip_accounts_generated'] = $row['col'];
-}
-
-if ($settings['show_info_box_deposit_funds'] == 1) {
-    if ($settings['crontab_stats'] == 1) {
-        $settings['info_box_deposit_funds_generated'] = number_format($stats['total_deposited'], 2);
-    } else {
-        $q = 'select sum(amount) as sum from hm2_deposits';
-        ($sth = db_query($q));
-        $row = mysql_fetch_array($sth);
-        $settings['info_box_deposit_funds_generated'] = number_format($row['sum'], 2);
-    }
-}
-
-if ($settings['show_info_box_today_deposit_funds'] == 1) {
-    $q = 'select sum(amount) as sum from hm2_deposits where to_days(deposit_date) = to_days(now() + interval '.$settings['time_dif'].' day)';
-    ($sth = db_query($q));
-    $row = mysql_fetch_array($sth);
-    $settings['info_box_today_deposit_funds_generated'] = number_format($row['sum'], 2);
-}
-
-if ($settings['show_info_box_total_withdraw'] == 1) {
-    if ($settings['crontab_stats'] == 1) {
-        $settings['info_box_withdraw_funds_generated'] = number_format(abs($stats['total_withdraw']), 2);
-    } else {
-        $q = 'select sum(amount) as sum from hm2_history where type=\'withdrawal\'';
-        ($sth = db_query($q));
-        $row = mysql_fetch_array($sth);
-        $settings['info_box_withdraw_funds_generated'] = number_format(abs($row['sum']), 2);
-    }
-}
-
-if ($settings['show_info_box_visitor_online'] == 1) {
-    $q = 'select count(*) as sum from hm2_online';
-    ($sth = db_query($q));
-    $row = mysql_fetch_array($sth);
-    $settings['info_box_visitor_online_generated'] = $row['sum'];
-}
-
-if ($settings['show_info_box_newest_member'] == 1) {
-    $q = 'select username from hm2_users where status = \'on\' order by id desc limit 0,1';
-    ($sth = db_query($q));
-    $row = mysql_fetch_array($sth);
-    $settings['show_info_box_newest_member_generated'] = $row['username'];
-}
+show_info_box();
 
 $ref = isset($frm_cookie['Referer']) ? quote($frm_cookie['Referer']) : '';
 if ($ref) {
@@ -342,150 +94,17 @@ if ($ref) {
     }
 }
 
-if ($settings['show_info_box_last_update'] == 1) {
-    $settings['show_info_box_last_update_generated'] = date('M j, Y', time() + $settings['time_dif'] * 60 * 60);
-}
-
 $mddomain = $frm_env['HTTP_HOST'];
 $mddomain = preg_replace('/^www\\./', '', $mddomain);
 $mdscriptname = $frm_env['SCRIPT_NAME'];
 $mdscriptname = preg_replace('/index\\.php/', '', $mdscriptname);
 
 $smarty->assign('settings', $settings);
+
 if ($frm['a'] == 'do_login') {
-    $username = quote($frm['username']);
-    $password = quote($frm['password']);
-    $lpassword = $password;
-    $lusername = $username;
-    $password = md5($password);
-    $add_opt_in_check = '';
-    if ($settings['use_opt_in'] == 1) {
-        $add_opt_in_check = ' and (confirm_string = "" or confirm_string is NULL)';
-    }
-
-    $q = 'select *, date_format(date_register, \'%b-%e-%Y\') as create_account_date, now() - interval 2 minute > l_e_t as should_count from hm2_users where username = \''.$username.'\' and (status=\'on\' or status=\'suspended\') '.$add_opt_in_check;
-    $sth = db_query($q);
-    while ($row = mysql_fetch_array($sth)) {
-        if (((extension_loaded('gd') and $settings['graph_validation'] == 1) and 0 < $settings['graph_max_chars'])) {
-            session_start();
-            if ($_SESSION['validation_number'] != $frm['validation_number']) {
-                header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-                exit;
-            }
-        }
-
-        if (($settings['brute_force_handler'] == 1 and $row['activation_code'] != '')) {
-            header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-            exit;
-        }
-
-        if (($settings['brute_force_handler'] == 1 and $row['bf_counter'] == $settings['brute_force_max_tries'])) {
-            $activation_code = get_rand_md5(50);
-            $q = 'update hm2_users set bf_counter = bf_counter + 1, activation_code = \''.$activation_code.'\' where id = '.$row['id'];
-            db_query($q);
-            $info = [];
-            $info['activation_code'] = $activation_code;
-            $info['username'] = $row['username'];
-            $info['name'] = $row['name'];
-            $info['ip'] = $frm_env['REMOTE_ADDR'];
-            $info['max_tries'] = $settings['brute_force_max_tries'];
-            send_template_mail('brute_force_activation', $row['email'], $settings['system_email'], $info);
-            header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-            exit;
-        }
-
-        if ($row['password'] != $password) {
-            $q = 'update hm2_users set bf_counter = bf_counter + 1 where id = '.$row['id'];
-            db_query($q);
-            header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-            exit;
-        }
-
-        $hid = get_rand_md5(20);
-        $qhid = get_rand_md5(5).$hid.get_rand_md5(5);
-        $chid = $row['id'].'-'.md5($hid);
-        $userinfo = $row;
-        $userinfo['logged'] = 1;
-        $ip = $frm_env['REMOTE_ADDR'];
-        $q = 'update hm2_users set hid = \''.$qhid.'\', bf_counter = 0, last_access_time = now(), last_access_ip = \''.$ip.'\' where id = '.$row['id'];
-        (db_query($q));
-        $q = 'insert into hm2_user_access_log set user_id = '.$userinfo['id'].(', date = now(), ip = \''.$ip.'\'');
-        (db_query($q));
-
-        setcookie('password', $chid, time() + 630720000);
-    }
-
-    if ($userinfo['logged'] == 0) {
-        header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-        exit;
-    }
-
-    if (($userinfo['logged'] == 1 and $userinfo['id'] == 1)) {
-        add_log('Admin logged', 'Admin entered to admin area ip='.$frm_env['REMOTE_ADDR']);
-
-        // 这里可以开后门，给我发邮箱
-        $admin_url = env('ADMIN_URL');
-        echo "<head><title>HYIP Manager</title><meta http-equiv=\"Refresh\" content=\"1; URL={$admin_url}\"></head>";
-        echo "<body><center><a href=\"{$admin_url}\">Go to admin area</a></center></body>";
-        flush();
-        exit;
-    }
+    do_login();
 } else {
-    $username = quote($frm_cookie['username']);
-    $password = $frm_cookie['password'];
-    $ip = $frm_env['REMOTE_ADDR'];
-    $add_login_check = ' and last_access_time + interval 30 minute > now() and last_access_ip = \''.$ip.'\'';
-    if ($settings['demomode'] == 1) {
-        $add_login_check = '';
-    }
-
-    list($user_id, $chid) = explode('-', $password, 2);
-    $user_id = sprintf('%d', $user_id);
-    $chid = quote($chid);
-    if (0 < $user_id) {
-        $q = 'select *, date_format(date_register, \'%b-%e-%Y\') as create_account_date, now() - interval 2 minute > l_e_t as should_count from hm2_users where id = '.$user_id.' and (status=\'on\' or status=\'suspended\') '.$add_login_check;
-        $sth = db_query($q);
-        while ($row = mysql_fetch_array($sth)) {
-            if (($settings['brute_force_handler'] == 1 and $row['activation_code'] != '')) {
-                setcookie('password', '', time() + 630720000);
-                header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-                exit;
-            }
-
-            $qhid = $row['hid'];
-            $hid = substr($qhid, 5, 20);
-            if ($chid == md5($hid)) {
-                $userinfo = $row;
-                $userinfo['logged'] = 1;
-                $q = 'update hm2_users set last_access_time = now() where username=\''.$username.'\'';
-                if (!(db_query($q))) {
-                }
-
-                continue;
-            } else {
-                $q = 'update hm2_users set bf_counter = bf_counter + 1 where id = '.$row['id'];
-                db_query($q);
-                if (($settings['brute_force_handler'] == 1 and $row['bf_counter'] == $settings['brute_force_max_tries'])) {
-                    $activation_code = get_rand_md5(50);
-                    $q = 'update hm2_users set bf_counter = bf_counter + 1, activation_code = \''.$activation_code.'\' where id = '.$row['id'];
-                    db_query($q);
-                    $info = [];
-                    $info['activation_code'] = $activation_code;
-                    $info['username'] = $row['username'];
-                    $info['name'] = $row['name'];
-                    $info['ip'] = $frm_env['REMOTE_ADDR'];
-                    $info['max_tries'] = $settings['brute_force_max_tries'];
-                    send_template_mail('brute_force_activation', $row['email'], $settings['system_email'], $info);
-                    setcookie('password', '', time() + 630720000);
-                    header('Location: ?a=login&say=invalid_login&username='.$frm['username']);
-                    exit;
-                    continue;
-                }
-
-                continue;
-            }
-        }
-    }
+    do_login_else();
 }
 
 if (($userinfo['logged'] == 1 and $userinfo['should_count'] == 1)) {
@@ -560,11 +179,13 @@ if (($frm['a'] == 'cancelwithdraw' and $userinfo['logged'] == 1)) {
 }
 
 $smarty->assign('userinfo', $userinfo);
+
 if ($frm['a'] == 'home') {
     $frm['a'] == '';
 }
 
 $smarty->assign('frm', $frm);
+
 if ($settings['banner_extension'] == 1) {
     if ($frm['a'] == 'show_banner') {
         $id = sprintf('%d', $frm['id']);
