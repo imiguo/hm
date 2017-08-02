ab1521d370b6a7971fd36fbd0e185f6d86726b01
entimm <entimm@gmail.com>
2017-08-03 00:00:56 +0800

    Fix

--- constants.php
+++ constants.php
@@ -15,6 +15,9 @@ define('THEME', $_SERVER['THEME'] ?? env('THEME') ?: 'default');
 
 define('TMPL_PATH', dirname(APP_PATH).'/templates/'.THEME.'/tmpl');
 
+// todo: 得到子域名
+define('SUBDOMAIN', '');
+
 define('CACHE_PATH', APP_PATH.'/tmpl_c');
 
 if ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) {
